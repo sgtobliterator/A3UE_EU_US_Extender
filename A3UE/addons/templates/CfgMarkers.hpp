43da2a7eb52294f72805bc5e7066fd6d08687b6c
Connor Walsh <connor.w.misc@gmail.com>
2025-09-26 18:45:37 -0400

    final initial

---
+++ A3UE/addons/templates/CfgMarkers.hpp
@@ -0,0 +1,32 @@
+class CfgMarkers 
+{
+	class flag_NATO;
+
+	class russian_flag : flag_NATO 
+	{
+		name = "Russian Forces";
+		icon = QPATHTOFOLDER(Pictures\Markers\ru_ico_co.paa);
+		texture = QPATHTOFOLDER(Pictures\Markers\ru_ico_co.paa);
+	};
+
+	class ypg_flag : flag_NATO 
+	{
+		name = "Yulakian Provisional Government";
+		icon = QPATHTOFOLDER(Pictures\Markers\ypg_ico_co.paa);
+		texture = QPATHTOFOLDER(Pictures\Markers\ypg_ico_co.paa);
+	};
+
+	class ylf_flag : flag_NATO 
+	{
+		name = "Yulakian Liberation Front";
+		icon = QPATHTOFOLDER(Pictures\Markers\ylf_ico_co.paa);
+		texture = QPATHTOFOLDER(Pictures\Markers\ylf_ico_co.paa);
+	};
+
+	class spz_flag : flag_NATO 
+	{
+		name = "Spetsnaz";
+		icon = QPATHTOFOLDER(Pictures\Markers\spz_ico_co.paa);
+		texture = QPATHTOFOLDER(Pictures\Markers\spz_ico_co.paa);
+	};
+};
